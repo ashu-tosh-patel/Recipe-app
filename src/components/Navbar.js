40b86a3591d7a0951f1bdb368a4d63f1c7d5ea14
ashu-tosh-patel <ashutoshpatelnoida@gmail.com>
2022-09-06 19:19:54 +0530

    Learned to use useContext hook and created own customized useTheme hook by using useContext to apply theme property in any app

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,24 @@
 import { Link } from 'react-router-dom'
+import { useTheme } from '../hooks/useTheme'
+
 
 //styles
 import './Navbar.css'
+
+//components
 import SearchBar from './SearchBar'
 
+
 export default function Navbar() {
+    const { color } = useTheme()
+
     return (
-        <div className="navbar">
+        <div className="navbar" style={{ background: color }}>
             <nav>
                 <Link to="/" className="brand">
                     <h1>Cooking Ashu</h1>
                 </Link>
-                <SearchBar/>
+                <SearchBar />
                 <Link to="/create">Create recipe</Link>
             </nav>
         </div>
